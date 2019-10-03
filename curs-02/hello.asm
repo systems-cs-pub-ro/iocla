f0daabe9a1a437f7fadae98e77260bd729d53910
Dragoș Niculescu <dragos.niculescu@cs.pub.ro>
2019-10-03 17:23:47 +0300

    added 04, 05

--- curs-02/hello.asm
+++ curs-02/hello.asm
@@ -12,21 +12,20 @@ section .text
 	global main
 	
 main:
-    mov ebp, esp; for correct debugging
-    push ebp           ; cadru de stivă adăugat de sasm
-    mov ebp, esp       ; for correct debugging
-	mov ax, 0x102      ; o constantă într-un registru de 16 biți
-                            ; AX = 258 în zecimal/decimal, 0x102 în hexa, 100000010 în binar
-	mov ah, -1         ; o constantă într-un registru de 8 biți
-	mov al, 1          ; AX este de fapt AH concatenat cu AL (high and low)
-adunare:                    ; etichetă
-	add al, ah         ; aritmetică pe 8 biți
-                            ; urmăriți registrul FLAGS
-                            ; *exercițiu*: încercați diverse valori, și observați ZF, CF, OF, PF 
-	mov eax, 0x1234ABCD; EAX este pe 32 biți, partea de jos (least significant) se numește AX
-                            ; în AX avem 0xABCD     
-       ; jmp iesire         ; decomenteaza pentru a sări peste print string   
-	and eax, 0xffff0000
+	push ebp           	; cadru de stivă adăugat de sasm
+	mov ebp, esp       	; for correct debugging
+	mov ax, 0x102      	; o constantă într-un registru de 16 biți
+				; AX = 258 în zecimal/decimal, 0x102 în hexa, 100000010 în binar
+	mov ah, -1         	; o constantă într-un registru de 8 biți
+	mov al, 1          	; AX este de fapt AH concatenat cu AL (high and low)
+adunare:                    	; etichetă
+	add al, ah         	; aritmetică pe 8 biți
+				; urmăriți registrul FLAGS
+				; *exercițiu*: încercați diverse valori, și observați ZF, CF, OF, PF 
+	mov eax, 0x1234ABCD	; EAX este pe 32 biți, partea de jos (least significant) se numește AX
+				; în AX avem 0xABCD     
+       ; jmp iesire         	; decomenteaza pentru a sări peste print string   
+	and eax, 0xffff0000 	; mască pentru a reseta la zero jumătatea de jos a lui EAX 
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, msg
@@ -35,5 +34,5 @@ adunare:                    ; etichetă
 
 iesire:
 	xor eax, eax       ; vom returna 0 către shell
-         pop ebp            ; refacem stiva
+        pop ebp            ; refacem stiva
 	ret                ; părăsim main 
