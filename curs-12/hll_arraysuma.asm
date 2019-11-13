efa3afedf2817396455552b6dfbe10a90ba68857
Dragoș Niculescu <dragos.niculescu@cs.pub.ro>
2019-11-13 13:58:21 +0200

    EBX should be saved by callee (cdecl)

--- curs-12/hll_arraysuma.asm
+++ curs-12/hll_arraysuma.asm
@@ -7,17 +7,18 @@ segment .text
 global  array_sum
 
 array_sum:
-      enter   0,0
-	push EBX		; UPB correction(VI) 
-      mov     EDX,[EBP+8]    ; copy array pointer to EDX
-      mov     ECX,[EBP+12]   ; copy array size to ECX
-      sub     EBX,EBX        ; array index = 0
-      sub     EAX,EAX        ; sum = 0 (EAX keeps the sum)
+	enter   0,0
+	push 	EBX		; UPB correction(VI) 
+	mov     EDX,[EBP+8]    	; copy array pointer to EDX
+	mov     ECX,[EBP+12]   	; copy array size to ECX
+	sub     EBX,EBX        	; array index = 0
+	sub     EAX,EAX        	; sum = 0 (EAX keeps the sum)
 add_loop:
-      add     EAX,[EDX+EBX*4]
-      inc     EBX            ; increment array index
-      cmp     EBX,ECX
-      jl      add_loop
-	pop EBX			; UPB correction(VI)
-      leave	
-      ret
+	add     EAX,[EDX+EBX*4]
+	inc     EBX            	; increment array index
+	cmp     EBX,ECX
+	jl      add_loop
+	pop 	EBX		; UPB correction(VI)
+	leave	
+	ret
+	
