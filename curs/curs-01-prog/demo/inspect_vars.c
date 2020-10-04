8ef4424e1ffb6ceedb4378966f8d29e9c35e25b0
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2020-10-05 01:11:50 +0300

    curs-01-prog: Add demo to inspect C variabiles

---
+++ curs/curs-01-prog/demo/inspect_vars.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 10;
+	char c = 'a';
+	char message[128];
+	int key[256];
+
+	printf("a (value): %d, a (address): %p, a (size): %zu\n", a, &a, sizeof(a));
+	printf("c (value): %hhd, a (address): %p, a (size): %zu\n", c, &c, sizeof(c));
+	printf("message (address): %p, message (size): %zu\n", message, sizeof(message));
+	printf("key (address): %p, key (size): %zu\n", key, sizeof(key));
+
+	return 0;
+}
