b4dd4e855df3c967a1a50f173429cfa819dd9d10
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2020-10-05 12:50:20 +0300

    curs-01-prog: Add demo to inspect C functions

---
+++ curs/curs-01-prog/demo/inspect_func.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+static void my_func(void)
+{
+	/* Do nothing, successfully. */
+}
+
+int main(void)
+{
+	void (*f)(void) = my_func;
+
+	printf("my_func: %p\n", my_func);
+	printf("&my_func: %p\n", my_func);
+	printf("main: %p\n", main);
+	printf("&main: %p\n", &main);
+	printf("f: %p\n", f);
+	printf("&f: %p\n", &f);
+
+	return 0;
+}
