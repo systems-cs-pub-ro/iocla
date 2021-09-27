7b7151aba0035927a82452aba616d9b11b519fc1
Andrei Albisoru <andreialbisoru98@gmail.com>
2021-09-27 10:01:13 +0300

    added regex based conversion

--- scripts/convert.py
+++ scripts/convert.py
@@ -10,16 +10,37 @@
 import dokuwiki
 import pypandoc
 import argparse
+import re
+
+tags = {
+    'NOTE'      : '<note> ',
+    'TIP'       : '<note tip> ',
+    'IMPORTANT' : '<note important> ',
+    'INFO'      : '<note info> ',
+    'WARNING'   : '<note warning> ',
+}
+
+lab_to_namespace_suffix = {
+    'laborator/content/reprezentare-numere'       : 'laborator-01',
+    'laborator/content/operatii-memorie-gdb'      : 'laborator-02',
+    'laborator/content/toolchain-decompilare'     : 'laborator-03',
+    'laborator/content/introducere-asamblare'     : 'laborator-04',
+    'laborator/content/rolul-registrelor-adresare': 'laborator-05',
+    'laborator/content/stiva'                     : 'laborator-06',
+    'laborator/content/apel-functii'              : 'laborator-07',
+    'laborator/content/structuri-vectori-siruri'  : 'laborator-08',
+    'laborator/content/interactiune-c-assembly'   : 'laborator-09',
+    'laborator/content/buffer-overflow'           : 'laborator-10',
+    'laborator/content/optimizari'                : 'laborator-11',
+    'laborator/content/linking'                   : 'laborator-12',
+}
+
+regexes = {}
+note_beg_regex = re.compile("^\s*>\s*")
+for tag, _ in tags.items():
+    regexes[tag] = re.compile("^\s*>\s*[*_]{2,3}\s*" + tag + "\s*:?\s*[*_]{2,3}\s*:?\s*")
 
 NAMESPACE     = 'iocla/laboratoare/'
-MDNOTE        = '> **NOTE**:'
-MDTIP         = '> **TIP**:'
-MDIMPORTANT   = '> **IMPORTANT**:'
-MDINFO        = '> **INFO**:'
-WIKINOTE      = '<note> '
-WIKITIP       = '<note tip> '
-WIKIINFO      = '<note info> '
-WIKIIMPORTANT = '<note important> '
 ENDNOTE       = ' </note>'
 
 parser = argparse.ArgumentParser(description='md to ocw format')
@@ -46,30 +67,25 @@ def convert_notes(content):
     in_form_prev = False
     for i in range(n):
         in_form_curr = False
-        if content[i] != '' and content[i][0] == '>':
+        if note_beg_regex.match(content[i]):
+            found_tag = False
             in_form_curr = True
-            if MDNOTE == content[i][:len(MDNOTE)]:
-                content[i] = WIKINOTE + content[i][len(MDNOTE):]
-
-            elif MDTIP == content[i][:len(MDTIP)]:
-                content[i] = WIKITIP + content[i][len(MDTIP):]
+            for tag, regex in regexes.items():
+                if regex.match(content[i]):
+                    found_tag = True
+                    content[i] = regex.sub(tags[tag], content[i])
+                    break
+                    
+            if not found_tag:
+                content[i] = note_beg_regex.sub('', content[i])
             
-            elif MDIMPORTANT == content[i][:len(MDIMPORTANT)]:
-                content[i] = WIKIIMPORTANT + content[i][len(MDIMPORTANT):]
-            
-            elif MDINFO == content[i][:len(MDINFO)]:
-                content[i] = WIKIINFO + content[i][len(MDINFO):]
-            
-            else:
-                content[i] = content[i][1:]
-        
         if not in_form_curr and in_form_prev:
-            content[i-1] = content[i-1] + ' </note>'
+            content[i-1] = content[i-1] + ENDNOTE
         
         in_form_prev = in_form_curr
     
     if in_form_prev:
-        content[n-1] = content[n-1] + ' </note>'
+        content[n-1] = content[n-1] + ENDNOTE
     
     return '\n'.join(content)
         
@@ -85,20 +101,6 @@ def update_pages(conn, page, content):
 
 
 def main():
-    lab_to_namespace_suffix = {
-        "laborator/content/reprezentare-numere"       : "laborator-01",
-        "laborator/content/operatii-memorie-gdb"      : "laborator-02",
-        "laborator/content/toolchain-decompilare"     : "laborator-03",
-        "laborator/content/introducere-asamblare"     : "laborator-04",
-        "laborator/content/rolul-registrelor-adresare": "laborator-05",
-        "laborator/content/stiva"                     : "laborator-06",
-        "laborator/content/apel-functii"              : "laborator-07",
-        "laborator/content/structuri-vectori-siruri"  : "laborator-08",
-        "laborator/content/interactiune-c-assembly"   : "laborator-09",
-        "laborator/content/buffer-overflow"           : "laborator-10",
-        "laborator/content/optimizari"                : "laborator-11",
-        "laborator/content/linking"                   : "laborator-12",
-    }
     args = vars(parser.parse_args())
     if args['username'] is not None and args['password'] is not None and args['files'] is not None:
         conn = create_connection(args['username'], args['password'])
