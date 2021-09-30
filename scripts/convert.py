847720f2b555292bd74bd58b5106c5fd07a57abd
Andrei Albisoru <andreialbisoru98@gmail.com>
2021-09-30 20:32:23 +0300

    simplify script

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-# Note:
+# DEPENDENCIES:
 #
-# In order to run install:
-#   1. pandoc   (sudo apt-get install pandoc).
-#   2. dokuwiki (pip install dokuwiki)
-#   3. pypandoc (pip install pypandoc)
+# 1. pandoc   (sudo apt-get install pandoc).
+# 2. dokuwiki (pip install dokuwiki)
+# 3. pypandoc (pip install pypandoc)
+#
+# IMPORTANT:
+# 
+# In order to upload to ocw directly 
 
 import dokuwiki
 import pypandoc
@@ -20,45 +23,23 @@ tags = {
     'WARNING'   : '<note warning> ',
 }
 
-lab_to_namespace_suffix = {
-    'laborator/content/reprezentare-numere'       : 'laborator-01',
-    'laborator/content/operatii-memorie-gdb'      : 'laborator-02',
-    'laborator/content/toolchain-decompilare'     : 'laborator-03',
-    'laborator/content/introducere-asamblare'     : 'laborator-04',
-    'laborator/content/rolul-registrelor-adresare': 'laborator-05',
-    'laborator/content/stiva'                     : 'laborator-06',
-    'laborator/content/apel-functii'              : 'laborator-07',
-    'laborator/content/structuri-vectori-siruri'  : 'laborator-08',
-    'laborator/content/interactiune-c-assembly'   : 'laborator-09',
-    'laborator/content/buffer-overflow'           : 'laborator-10',
-    'laborator/content/optimizari'                : 'laborator-11',
-    'laborator/content/linking'                   : 'laborator-12',
-}
-
 regexes = {}
 note_beg_regex = re.compile("^\s*>\s*")
 for tag, _ in tags.items():
     regexes[tag] = re.compile("^\s*>\s*[*_]{2,3}\s*" + tag + "\s*:?\s*[*_]{2,3}\s*:?\s*")
 
-NAMESPACE     = 'iocla/laboratoare/'
-ENDNOTE       = ' </note>'
+NAMESPACE = 'iocla/laboratoare/'
+ENDNOTE   = ' </note>'
 
-parser = argparse.ArgumentParser(description='md to ocw format')
-parser.add_argument('-o', '--output', help='output')
-parser.add_argument('-u', '--username', help='user')
-parser.add_argument('-p', '--password', help='password')
-parser.add_argument(
-    '-f',
-    '--files',
-    nargs='+',
-    help='markdown files to publish, all files should be named README.md')
+parser = argparse.ArgumentParser(description='converts a markdown file to wiki format')
+parser.add_argument('-u', '--username',  help='username for ocw account (e.g. gigel)')
+parser.add_argument('-p', '--password',  help='password for ocw account (e.g. secret)')
+parser.add_argument('-n', '--namespace', help='provide the namespace that needs to be updated (e.g laborator-01, laborator-02, ...)')
+parser.add_argument('-f', '--file',      help='markdown files to publish, all files should be named README.md', required=True)
 
 
 def create_connection(username, password):
-    return dokuwiki.DokuWiki('https://ocw.cs.pub.ro/courses',
-                             username,
-                             password,
-                             cookieAuth=True)
+    return dokuwiki.DokuWiki('https://ocw.cs.pub.ro/courses', username, password, cookieAuth=True)
 
 def convert_notes(content):
     content = content.split('\n')
@@ -95,31 +76,23 @@ def update_pages(conn, page, content):
         conn.pages.set(NAMESPACE + page, content)
     except (dokuwiki.DokuWikiError, Exception) as err:
         # ocw doesn't send always valid reply
-        if not str(err).startswith(
-                'XML or text declaration not at start of entity'):
+        if not str(err).startswith('XML or text declaration not at start of entity'):
             print('unable to process: %s' % err)
 
 
 def main():
     args = vars(parser.parse_args())
-    if args['username'] is not None and args['password'] is not None and args['files'] is not None:
-        conn = create_connection(args['username'], args['password'])
-        files = args['files']
-        for file in files:
-            output = convert_notes(pypandoc.convert_file(file, 'dokuwiki', format='gfm', extra_args=['--wrap=preserve']))
-            namespace_suffix = lab_to_namespace_suffix[file.replace('/README.md', '')]
-            update_pages(conn, namespace_suffix, output)
-    elif args['files'] is not None and len(args['files']) == 1:
-        if args['output'] is not None:
-            fout = args['output']
-        else:
-            fout = 'README.doku'
-        output = convert_notes(pypandoc.convert_file(args['files'][0], 'dokuwiki', format='gfm', extra_args=['--wrap=preserve']))
-        with open(fout, 'w') as f:
-            f.write(output)
-    else:
-        print('invalid combination of arguments')
 
+    if args['username'] is not None and args['password'] is not None and args['namespace'] is not None:
+        # If username, password and namespace are provided upload in ocw
+        conn   = create_connection(args['username'], args['password'])
+        output = convert_notes(pypandoc.convert_file(args['file'], 'dokuwiki', format='gfm', extra_args=['--wrap=preserve']))
+        update_pages(conn, args['namespace'], output)
+    else:
+        # Otherwise convert the file to wiki format
+        output = convert_notes(pypandoc.convert_file(args['file'], 'dokuwiki', format='gfm', extra_args=['--wrap=preserve']))
+        with open('README.doku', 'w') as f:
+            f.write(output)
 
 if __name__ == '__main__':
     main()
--- scripts/update-ocw.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-python -m pip install dokuwiki
-python -m pip install pypandoc
-
-
-python -c "from pypandoc.pandoc_download import download_pandoc;download_pandoc(version='2.5')"
-
-files=`git diff --name-only HEAD~1 HEAD | grep README.md`
-python scripts/convert.py --username $USERNAME --password $PASSWORD --files $files
