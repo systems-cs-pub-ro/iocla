01f2f8b53fa360c320af02dfe8e1c9c812b1633f
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2021-10-11 11:20:11 +0300

    01-prog: Make variables global in inspect_vars.c
    Signed-off-by: Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>

--- curs/chap-01-prog/01-pointers/inspect_vars.c
+++ curs/chap-01-prog/01-pointers/inspect_vars.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
+long a = 10;
+char c = 'a';
+char message[128];
+int key[256];
+
 int main(void)
 {
-  long a = 10;
-  char c = 'a';
-  char message[128];
-  int key[256];
+	printf("a (value): %ld, a (address): %p, a (size): %zu\n", a, &a, sizeof(a));
+	printf("c (value): %hhd, a (address): %p, a (size): %zu\n", c, &c, sizeof(c));
+	printf("message (address): %p (%p), message (size): %zu\n", message, &message, sizeof(message));
+	printf("key (address): %p (%p), key (size): %zu\n", key, &key, sizeof(key));
+	printf("main (address): %p (%p), main (size): %zu\n", main, &main, sizeof(main));
+	printf("sizeof(&a): %zu, sizeof(&key): %zu\n", sizeof(&a), sizeof(&key));
 
-  printf("a (value): %ld, a (address): %p, a (size): %zu\n", a, &a, sizeof(a));
-  printf("c (value): %hhd, a (address): %p, a (size): %zu\n", c, &c, sizeof(c));
-  printf("message (address): %p, message (address): %p, message (size): %zu\n",
-	 message, &message, sizeof(message));
-  printf("key (address): %p, key (size): %zu\n", key, sizeof(key));
-
-  return 0;
+	return 0;
 }
