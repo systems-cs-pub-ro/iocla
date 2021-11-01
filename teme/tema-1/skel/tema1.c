fc67a2624c6bf2f05c1c9f7c413b4fd2b9a1cc24
Tudor Hodoboc-Velescu <tudorhodoboc@yahoo.com>
2021-11-01 14:38:22 +0200

    Adaugare varianta finala v3

--- teme/tema-1/skel/tema1.c
+++ teme/tema-1/skel/tema1.c
@@ -19,27 +19,27 @@ typedef struct File {
 	struct File* next;
 } File;
 
-void touch (Dir parent, char* name) {}
+void touch (Dir* parent, char* name) {}
 
-void mkdir (Dir parent, char* name) {}
+void mkdir (Dir* parent, char* name) {}
 
-void ls (Dir parent) {}
+void ls (Dir* parent) {}
 
-void rm (Dir parent, char* name) {}
+void rm (Dir* parent, char* name) {}
 
-void rmdir (Dir parent, char* name) {}
+void rmdir (Dir* parent, char* name) {}
 
-void cd(Dir *target, char *name) {}
+void cd(Dir** target, char *name) {}
 
-char *pwd (Dir target) {}
+char *pwd (Dir* target) {}
 
-void stop (Dir target) {}
+void stop (Dir* target) {}
 
-void tree (Dir target, int level) {}
+void tree (Dir* target, int level) {}
 
-void find(Dir parent, char *name, int level) {}
+void find(Dir* parent, char *name, int level) {}
 
-void mv(Dir parent, char *oldname, char *newname) {}
+void mv(Dir* parent, char *oldname, char *newname) {}
 
 int main () {
 	do
