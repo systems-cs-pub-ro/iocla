bc328e791d8f5b9276a9f29aa59b6ad4d970fbbe
cristian-vijelie <cristianvijelie@gmail.com>
2021-12-15 08:59:44 +0200

    Add explanations in the Task4 skel

--- teme/tema-3/task-4/task4.asm
+++ teme/tema-3/task-4/task4.asm
@@ -4,6 +4,13 @@ global expression
 global term
 global factor
 
+; `factor(char *p, int *i)`
+;       Evaluates "(expression)" or "number" expressions 
+; @params:
+;	p -> the string to be parsed
+;	i -> current position in the string
+; @returns:
+;	the result of the parsed expression
 factor:
         push    ebp
         mov     ebp, esp
@@ -11,6 +18,13 @@ factor:
         leave
         ret
 
+; `term(char *p, int *i)`
+;       Evaluates "factor" * "factor" or "factor" / "factor" expressions 
+; @params:
+;	p -> the string to be parsed
+;	i -> current position in the string
+; @returns:
+;	the result of the parsed expression
 term:
         push    ebp
         mov     ebp, esp
@@ -18,6 +32,13 @@ term:
         leave
         ret
 
+; `expression(char *p, int *i)`
+;       Evaluates "term" + "term" or "term" - "term" expressions 
+; @params:
+;	p -> the string to be parsed
+;	i -> current position in the string
+; @returns:
+;	the result of the parsed expression
 expression:
         push    ebp
         mov     ebp, esp
