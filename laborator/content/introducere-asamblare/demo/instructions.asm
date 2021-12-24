dfc043e870393d08322c889eda4ae712e405f711
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2021-12-24 16:13:30 +0200

    Add demos for lab05.
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

---
+++ laborator/content/introducere-asamblare/demo/instructions.asm
@@ -0,0 +1,55 @@
+;
+; Author: Gabriel Mocanu <gabi.mocanu98@gmail.com>
+;
+
+
+%include "../io.mac"
+
+section .text
+    global main
+    extern printf
+
+main:
+    push 0
+    popf
+
+    mov eax, 0
+    test eax, eax
+
+    push 0
+    popf
+
+    mov ebx, 0xffffffff
+    test ebx, ebx
+
+    push 0
+    popf
+
+    mov al, 250
+    mov bl, 10
+    add al, bl
+
+    push 0
+    popf
+
+    mov al, 0
+    mov bl, 1
+    sub al, bl
+
+    push 0
+    popf
+
+    mov al, 120
+    mov bl, 120
+    add al, bl
+
+    push 0
+    popf
+
+    mov al, 129
+    mov bl, 129
+    add al, bl
+
+
+    PRINTF32 `%d\n\x0`, eax
+    ret
