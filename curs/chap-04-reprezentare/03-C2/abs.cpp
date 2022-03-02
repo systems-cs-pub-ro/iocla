d6d3dd9eaaa2608dadc7a864d13fac36ee2facbe
Dragoș Niculescu <dragos.niculescu@cs.pub.ro>
2022-03-02 19:22:24 +0200

    removed .o files, fixed abs.cpp

--- curs/chap-04-reprezentare/03-C2/abs.cpp
+++ curs/chap-04-reprezentare/03-C2/abs.cpp
@@ -3,14 +3,16 @@
 int main()
 
 {
-  int x = 0x80000000;
-  std::cout << std::abs(x) << std::endl;
-  if(std::abs(x) < 0){
-    std::cout << "x is negative";  
+  int x = 0x80000000, modx;
+
+  modx = std::abs(x);
+  std::cout << "x = " << modx << std::endl;
+  if(modx < 0){
+    std::cout << "|x| is negative\n";  
   }
 }
 
 /* 
-   The situation is the same with java.Math.abs() 
-   and possibly other languages as well
+   The situation is the same with java.Math.abs(), and matlab;
+   python3 works well - extends representation as needed
 */
