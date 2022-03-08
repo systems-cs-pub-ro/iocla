4f3e60442ef724726d8aa456be24e10a424e6665
TheFlameStar <anca_negoita@yahoo.com>
2022-03-09 00:01:49 +0200

    Update rotations.c

--- laborator/content/reprezentare-numere/4-rotations/rotations.c
+++ laborator/content/reprezentare-numere/4-rotations/rotations.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 
-void rotate_left(int *number, int bits)
+void rotate_left(int* number, int bits)
 {
 	/* TODO */
-	(void) number;
-	(void) bits;
+	int number_shifted = sizeof(int) * 8 - bits, first_shift = *number << bits; 
+	int last_shift = *number >> number_shifted;
+	*number = first_shift + last_shift;
 }
 
 void rotate_right(int *number, int bits)
 {
 	/* TODO */
-	(void) number;
-	(void) bits;
+	int number_shifted = sizeof(int) * 8 - bits, first_shift = *number >> bits;
+	int last_shift = *number << number_shifted;
+	*number = first_shift + last_shift;
 }
 
 int main()
 {
 	/* TODO: Test functions */
-
+	int bits, number;
+	char type[20];
+	scanf("%d %d %s", &number, &bits, type);
+	if (strcmp(type, "left") == 0)
+		rotate_left(&number, bits);
+	else
+		rotate_right(&number, bits);
 	return 0;
 }
 
