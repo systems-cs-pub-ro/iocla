0b414efbb9e2086db1a00d271fcc80c0c3026f7d
TheFlameStar <anca_negoita@yahoo.com>
2022-03-08 23:47:37 +0200

    Update mirror.c

--- laborator/content/reprezentare-numere/3-mirror/mirror.c
+++ laborator/content/reprezentare-numere/3-mirror/mirror.c
@@ -2,16 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-void mirror(char *s)
+void mirror(char* s)
 {
 	/* TODO */
-	(void) s;
+	int l = strlen(s);
+	for (int i = 0; i < l / 2; ++i) {
+		char str = *(s + i);
+		*(s + i) = *(s + l - i - 1);
+		*(s + l - i - 1) = str;
+	}
+	printf("%s\n", s);
 }
 
 int main()
 {
 	/* TODO: Test function */
-
+	char s[100];
+	scanf("%s", s);
+	mirror(s);
 	return 0;
 }
 
