0cb624d537366e9e82dcc82838237dbd533be588
matthriscu <67328225+matthriscu@users.noreply.github.com>
2022-03-08 23:36:33 +0200

    .

--- laborator/content/reprezentare-numere/2-len_xor/len_xor.c
+++ laborator/content/reprezentare-numere/2-len_xor/len_xor.c
@@ -2,24 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NMAX 100
+
 int my_strlen(const char *str)
 {
-	/* TODO */
-	(void) str;
-
-	return -1;
+	const char *p = str;
+	while(*(p++));
+	return p - str - 1;
 }
 
 void equality_check(const char *str)
 {
-	/* TODO */
-	(void) str;
+	for (int i = 0, l = my_strlen(str); i < l; ++i)
+		// if (str[i] == str[(i + 2^i) % l])
+		if (!(*(str + i) ^ *(str + (i + (1 << i)) % l)))
+			printf("Address of %c: %p\n", *(str + i), str + i);
 }
 
 int main(void)
 {
-	/* TODO: Test functions */
-
+	char s[NMAX + 1];
+	scanf("%s", &s);
+	printf("length = %d\n", my_strlen(s));
+	equality_check(s);
 	return 0;
 }
 
