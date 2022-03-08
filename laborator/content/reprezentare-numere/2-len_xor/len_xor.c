1f165ac511aed24557e5dde30fe1c106dcb3342b
TheFlameStar <anca_negoita@yahoo.com>
2022-03-08 23:35:38 +0200

    Update len_xor.c

--- laborator/content/reprezentare-numere/2-len_xor/len_xor.c
+++ laborator/content/reprezentare-numere/2-len_xor/len_xor.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-int my_strlen(const char *str)
+int my_strlen(const char* str)
 {
 	/* TODO */
-	(void) str;
-
-	return -1;
+    int nr = 0;
+    while (*(str + nr++));
+	return nr - 1;
 }
 
-void equality_check(const char *str)
+void equality_check(const char* str)
 {
 	/* TODO */
-	(void) str;
+	int p = 1;
+	int n = my_strlen(str);
+	for (int i = 0; i < n; ++i) {
+		if (!(*(str + i) ^ *(str + ((i + p) % n))))
+			printf("The address of %c is %p\n", *(str + i), str + i);
+        p <<= 1;
+	}
 }
 
 int main(void)
 {
 	/* TODO: Test functions */
-
+	char str[100];
+	scanf("%s", str);
+	printf("The length of str is %d \n", my_strlen(str));
+	equality_check(str);
 	return 0;
 }
-
