88261da30405be0b816c0df3df5e914389e365f5
Violeta2002 <comsa.iulianavioleta@gamil.com>
2022-03-30 22:38:46 +0300

    Laborator 4: exercise 3: Add solutions for exercise 3 I solved the problem of finding the maximum in an array and the binary search using goto insted of repetitive instructions.
    Signed-off-by: Violeta2002 <comsa.iulianavioleta@gamil.com>

--- laborator/content/toolchain-decompilare/3-goto-algs/binary_search.c
+++ laborator/content/toolchain-decompilare/3-goto-algs/binary_search.c
@@ -6,9 +6,27 @@ int main(void)
 	int dest = v[2]; /* 15 */
 	int start = 0;
 	int end = sizeof(v) / sizeof(int) - 1;
+	int mid;
+	int pos = -1;
 
-	/* TODO: Implement binary search */
-	(void) dest;
-	(void) start;
-	(void) end;
+again:
+	
+if(start > end)
+	goto out;
+mid = start + (end - start) / 2;
+
+if(v[mid] == dest) {
+	pos = mid;
+	goto out;
+}
+
+if(v[mid] < dest) {
+	start = mid + 1;
+	goto again;
+}else if(dest < v[mid]) {
+end = mid - 1;
+goto again; }
+
+out: 
+	printf("%d", pos);
 }
--- laborator/content/toolchain-decompilare/3-goto-algs/vector_max.c
+++ laborator/content/toolchain-decompilare/3-goto-algs/vector_max.c
@@ -5,11 +5,20 @@ int main(void)
 	int v[] = {4, 1, 2, -17, 15, 22, 6, 2};
 	int max;
 	int i;
-
+	int dim = sizeof(v)/sizeof(v[0]);
 	/* TODO: Implement finding the maximum value in the vector */
 	max = v[0];
 	i = 1;
 
-	(void) i;
-	(void) max;
+change_max:
+	max = v[i];
+again:
+if(i == dim)
+	goto out;
+if(max < v[i])
+	goto change_max;
+i++;
+goto again;
+out:
+printf("%d\n", max);
 }
