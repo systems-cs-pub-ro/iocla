1f4624a0fec51da3109036c3458fb174f71663a4
Diana Cismaru <cismaru_diana02@yahoo.com>
2022-04-05 21:16:46 +0300

    lab5: Add solution for BONUS exercise 6 - Fibonacci
    Signed-off-by: Diana Cismaru <cismaru_diana02@yahoo.com>

--- laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
+++ laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
@@ -6,7 +6,23 @@ section .text
 
 main:
     mov eax, 7       ; vrem sa aflam al N-lea numar; N = 7
+    cmp eax, 1
+    je end
+    cmp eax, 0
+    je end
 
     ; TODO: calculati al N-lea numar fibonacci (f(0) = 0, f(1) = 1)
+    mov ebx, 0
+    mov ecx, 1
 
+fibo:
+    xchg ebx, ecx
+    add ecx, ebx
+    sub eax, 1
+    cmp eax, 1
+    ja fibo
+    mov eax, ecx
+
+end:
+    PRINTF32 `%d\n\x0`, eax
     ret
