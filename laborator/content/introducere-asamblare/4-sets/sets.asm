a3d95c96a1dd88bd5b068c472b4b8b805244af16
Diana Cismaru <cismaru_diana02@yahoo.com>
2022-04-05 20:42:16 +0300

    lab5: Add solution for exercise 4 - sets
    Signed-off-by: Diana Cismaru <cismaru_diana02@yahoo.com>

--- laborator/content/introducere-asamblare/4-sets/sets.asm
+++ laborator/content/introducere-asamblare/4-sets/sets.asm
@@ -12,22 +12,35 @@ main:
     PRINTF32 `%u\n\x0`, ebx ; afiseaza cea de-a doua multime
 
     ; TODO1: reuniunea a doua multimi
-
+    mov ecx, eax
+    or ecx, ebx
+    PRINTF32 `%u\n\x0`, ecx ; afiseaza prima multime
 
     ; TODO2: adaugarea unui element in multime
-
+    mov ecx, eax
+    or ecx, 0x0100
+    PRINTF32 `%u\n\x0`, ecx ; afiseaza prima multime
 
     ; TODO3: intersectia a doua multimi
-
+    mov ecx, eax
+    and ecx, ebx
+    PRINTF32 `%u\n\x0`, ecx ; afiseaza prima multime
 
     ; TODO4: complementul unei multimi
-
+    mov ecx, eax
+    not ecx
+    PRINTF32 `%u\n\x0`, ecx ; afiseaza prima multime
 
     ; TODO5: eliminarea unui element
-
+    mov ecx, eax
+    sub ecx, 0x0100
+    PRINTF32 `%u\n\x0`, ecx ; afiseaza prima multime
 
     ; TODO6: diferenta de multimi EAX-EBX
-
+    mov ecx, ebx
+    and ecx, eax
+    xor edx, ecx
+    PRINTF32 `%u\n\x0`, edx
 
     xor eax, eax
     ret
