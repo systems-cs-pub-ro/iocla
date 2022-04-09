e9b85dbcfb08ee1816a92b2d61f9413a8882bf05
George Raducanu <georgecristian2002@gmail.com>
2022-04-09 16:45:41 +0300

    lab6: ex0 Recap solved
    Signed-off-by: George Raducanu <georgecristian2002@gmail.com>

--- laborator/content/rolul-registrelor-adresare/0-recap/power-2.asm
+++ laborator/content/rolul-registrelor-adresare/0-recap/power-2.asm
@@ -8,9 +8,21 @@ main:
     mov ebp, esp
 
     mov eax, 211    ; to be broken down into powers of 2
-    mov ebx, 1      ; stores the current power
+    mov ebx, 0      ; stores the current power
 
     ; TODO - print the powers of 2 that generate number stored in EAX
+    mov ecx, 1
+    mov esi, 1
+start:
+    test eax, ecx
+    jz next
+    PRINTF32 `%d \x0`, ecx
+next:
+	shl ecx, 1
+	inc esi
+	cmp esi, 32
+	jne start
 
+PRINTF32 `\n \x0`
     leave
     ret
