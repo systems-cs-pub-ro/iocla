1cb4a96da8c02f69b9ac72be1ade063721d9addf
Vlad Negoita <vlad1negoita@gmail.com>
2022-04-30 00:53:05 +0300

    Solved lab8 - ex2

--- laborator/content/stiva/2-reverse/reverse-array.asm
+++ laborator/content/stiva/2-reverse/reverse-array.asm
@@ -14,11 +14,24 @@ global main
 main:
 
     ; TODO push the elements of the array on the stack
+    mov ecx, ARRAY_LEN
+label1:
+    mov eax, dword [input + 4 * (ecx - 1)]
+    push eax
+    loop label1
+
     ; TODO retrieve the elements (pop) from the stack into the output array
+    mov ecx, ARRAY_LEN
+label2:
+    pop eax
+    mov dword [output + 4 * (ecx - 1)], eax
+    loop label2
 
     PRINTF32 `Reversed array: \n\x0`
     xor ecx, ecx
+
 print_array:
+
     mov edx, [output + 4 * ecx]
     PRINTF32 `%d\n\x0`, edx
     inc ecx
