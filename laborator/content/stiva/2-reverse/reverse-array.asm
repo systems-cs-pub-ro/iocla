c6415180573d16d7d51157c626afee402e12a988
TheFlameStar <anca_negoita@yahoo.com>
2022-04-29 11:24:31 +0300

    ex 2

--- laborator/content/stiva/2-reverse/reverse-array.asm
+++ laborator/content/stiva/2-reverse/reverse-array.asm
@@ -12,9 +12,20 @@ section .text
 extern printf
 global main
 main:
-
+    mov ecx, ARRAY_LEN
     ; TODO push the elements of the array on the stack
     ; TODO retrieve the elements (pop) from the stack into the output array
+
+push_all:
+    push dword[input + 4 * ecx - 4]
+    loop push_all
+
+    mov ecx, ARRAY_LEN
+
+pop_all:
+    pop dword[output + 4 * ecx - 4]
+    loop pop_all
+
 
     PRINTF32 `Reversed array: \n\x0`
     xor ecx, ecx
