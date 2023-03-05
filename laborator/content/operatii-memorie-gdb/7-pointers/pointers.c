0ccd8c811ca2e530ff1569be4079416ae0c6af06
Robert Constantinescu <91455746+RCX777@users.noreply.github.com>
2023-03-05 15:20:14 +0200

    Update pointers.c after feedback on PR; fixes #121

--- laborator/content/operatii-memorie-gdb/7-pointers/pointers.c
+++ laborator/content/operatii-memorie-gdb/7-pointers/pointers.c
@@ -5,10 +5,6 @@
 
 #define SIGN(X) (((X) > 0) - ((X) < 0))
 
-int test_strcpy(const char *src, size_t size);
-int test_memcpy(const char *src, size_t size);
-
-
 int my_strcmp(const char *s1, const char *s2);
 void *my_memcpy(void *dest, const void *src, size_t n);
 char *my_strcpy(char *dest, const char *src);
@@ -17,6 +13,9 @@ int main() {
 	char s1[] = "Abracadabra";
 	char s2[] = "Abracababra";
 	char src[] = "Learn IOCLA, you must!";
+
+	char* dest_strcpy = malloc(sizeof(src));
+	char* dest_memcpy = malloc(sizeof(src));
 
 	(void) s1;
 	(void) s2;
@@ -27,51 +26,10 @@ int main() {
 	cu succes. In caz contrar, programul va crapa.
 	*/
 	// assert(SIGN(my_strcmp(s1, s2)) == SIGN(strcmp(s1, s2)));
-	// assert(test_strcpy(src, sizeof(src)));
-	// assert(test_memcpy(src, sizeof(src)));
+	// assert(strcpy(dest_strcpy, src) && !strcmp(src, dest_strcpy));
+	// assert(my_memcpy(dest_memcpy, src, sizeof(src)) && !memcmp(src, dest_memcpy, sizeof(src)));
 
+	free(dest_strcpy);
+	free(dest_memcpy);
 	return 0;
-}
-
-
-/* USED FOR TESTING! DO NOT MODIFY! */
-
-int test_strcpy(const char *src, size_t size) {
-	char *dest1 = malloc(size);
-	char *dest2 = malloc(size);
-
-	strcpy(dest1, src);
-	my_strcpy(dest2, src);
-
-	if (strcmp(dest1, dest2)) {
-		free(dest1);
-		free(dest2);
-		return 0;
-	}
-
-	free(dest1);
-	free(dest2);
-
-	return 1;
-}
-
-int test_memcpy(const char *src, size_t size) {
-	char *dest1 = malloc(size);
-	char *dest2 = malloc(size);
-
-	memcpy(dest1, src, size);
-	my_memcpy(dest2, src, size);
-
-	for (size_t i = 0; i < size; ++i) {
-		if (*dest1 != *dest2) {
-			free(dest1);
-			free(dest2);
-			return 0;
-		}
-	}
-
-	free(dest1);
-	free(dest2);
-
-	return 1;
 }
