ba62a3086ca8e2416249b4ad3d74288e76e5f069
Teodor Dutu <teodor.dutu@gmail.com>
2023-03-17 16:37:23 +0800

    lab/number-representation: Explain the need to cast parameters to `void`
    It is enough to add this explanation to the first lab so as to not
    pollute the skeleton code too much.
    
    Signed-off-by: Teodor Dutu <teodor.dutu@gmail.com>

--- laborator/content/reprezentare-numere/2-len_xor/len_xor.c
+++ laborator/content/reprezentare-numere/2-len_xor/len_xor.c
@@ -5,6 +5,13 @@
 int my_strlen(const char *str)
 {
 	/* TODO */
+
+	/**
+	 * The cast to (void) is used to avoid a compiler warning. Remove the line
+	 * below to find out what the warning is.
+	 *
+	 * Remove this cast when implementing the function.
+	 */
 	(void) str;
 
 	return -1;
