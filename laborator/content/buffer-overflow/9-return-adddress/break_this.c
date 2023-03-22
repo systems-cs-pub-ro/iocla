cac55ee64c040568ca7c39a705d02ee56d52807a
Robert <georobi.016@gmail.com>
2023-03-22 16:31:01 +0200

    lab/buffer-overflow: Chang to tab indent
    - Change break_this.c to tab-indenting

--- laborator/content/buffer-overflow/9-return-adddress/break_this.c
+++ laborator/content/buffer-overflow/9-return-adddress/break_this.c
@@ -6,40 +6,40 @@
 
 void magic_function(void)
 {
-   system("cowsay -f tux 'Good job there, my friend'");
+	system("cowsay -f tux 'Good job there, my friend'");
 }
 
 char read_buffer(void)
 {
-   int n;
-   unsigned int disorienting_var = 0xDEADBEEF;
-   char buffer[64] = "\0";
-   size_t i, len;
+	int n;
+	unsigned int disorienting_var = 0xDEADBEEF;
+	char buffer[64] = "\0";
+	size_t i, len;
 
-   printf("insert buffer length: ");
-   scanf("%d", &n);
-   getc(stdin);
+	printf("insert buffer length: ");
+	scanf("%d", &n);
+	getc(stdin);
 
-   printf("\nn is %d\n", n);
-   printf("insert buffer string: ");
-   fgets(buffer, n, stdin);
+	printf("\nn is %d\n", n);
+	printf("insert buffer string: ");
+	fgets(buffer, n, stdin);
 
-   printf("\nn is %x\n", n);
-   printf("buffer is: ");
-   len = strlen(buffer);
-   for (i = 0; i < len + 2; i++)
-      printf(" %02X(%c)", buffer[i], buffer[i]);
-   puts("");
+	printf("\nn is %x\n", n);
+	printf("buffer is: ");
+	len = strlen(buffer);
+	for (i = 0; i < len + 2; i++)
+		printf(" %02X(%c)", buffer[i], buffer[i]);
+	puts("");
 
-   printf("stack variable: %X\n", disorienting_var);
-   buffer[2] = disorienting_var;
+	printf("stack variable: %X\n", disorienting_var);
+	buffer[2] = disorienting_var;
 
-   return buffer[0];
+	return buffer[0];
 }
 
 int main(void)
 {
-   read_buffer();
+	read_buffer();
 
-   return 0;
+	return 0;
 }
