d1ebaf5e36e3696daf1dd16b8150372eb2c3763f
A0Dev0Knight <al.dev.knight@gmail.com>
2024-03-07 09:47:36 +0200

    Am facut si exercitiul 2

--- laborator/content/reprezentare-numere/2-len_xor/len_xor.c
+++ laborator/content/reprezentare-numere/2-len_xor/len_xor.c
@@ -14,6 +14,11 @@ int my_strlen(const char *str)
 	 */
 	(void) str;
 
+	// size
+	int size = strlen(str);
+	size --;
+	printf("length = %d\n",size);
+
 	return -1;
 }
 
@@ -21,12 +26,49 @@ void equality_check(const char *str)
 {
 	/* TODO */
 	(void) str;
+		// adresa 
+	int i;
+	int size = strlen(str);
+	size --;
+	for (i = 0; i < size; i++)
+	{
+		int putere = Pow(2,i);
+		if (!(*(str+i) ^ *(str + (i + putere)%size)))
+		{
+			printf("Address of %c: %p\n",*(str+i),str+i);
+			printf("Address of %c: %p\n",*(str + (i + putere)%size),(str + (i + putere)%size));
+			printf("\n");
+		}
+	}
+
+}
+
+int Pow(int baza, int putere)
+{
+	int p = 1;
+	if (putere == 0)
+	{
+		return 1;
+	}
+	
+	while (putere)
+	{
+		p*=baza;
+		putere--;
+	}
+	return p;
 }
 
 int main(void)
 {
 	/* TODO: Test functions */
 
+	char str[20];
+	fgets(str,20,stdin);
+	my_strlen(str);
+	equality_check(str);
+
+	
 	return 0;
 }
 
