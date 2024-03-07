26d50264752fea69f131cd80dde48414b468a955
A0Dev0Knight <al.dev.knight@gmail.com>
2024-03-07 09:59:26 +0200

    Am sters ceva inutil din codul problemei 3.

--- laborator/content/reprezentare-numere/3-mirror/mirror.c
+++ laborator/content/reprezentare-numere/3-mirror/mirror.c
@@ -6,12 +6,30 @@ void mirror(char *s)
 {
 	/* TODO */
 	(void) s;
+
+	char *copy;
+	copy=strdup(s);
+
+	int size = strlen(s);
+	int i;
+	for (i = 0; i < size; i++)
+	{
+		*(copy+i) = *(s+size-1-i);		
+	}
+
+	strcpy(s,copy);
+	free(copy);
+	printf("%s\n",s);
+	
 }
 
 int main(void)
 {
 	/* TODO: Test function */
+	char s[20];
+	scanf("%s",s);
 
+	mirror(s);
 	return 0;
 }
 
