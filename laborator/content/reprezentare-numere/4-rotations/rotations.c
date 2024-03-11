b2563b222d3e9fcd51054029301518e81c075148
A0Dev0Knight <al.dev.knight@gmail.com>
2024-03-11 12:04:46 +0200

    Am facut si problema 4, dar nu stiu de ce vu merge pentru numere foarte mari precum 0x80000000. Pe numere micute merge bine codul.

--- laborator/content/reprezentare-numere/4-rotations/rotations.c
+++ laborator/content/reprezentare-numere/4-rotations/rotations.c
@@ -3,21 +3,34 @@
 void rotate_left(unsigned int *number, int bits)
 {
 	/* TODO */
-	(void) number;
-	(void) bits;
+	unsigned int inumber = *number;
+	unsigned int rezult = (inumber << bits) | (inumber >> (32-bits));
+	*number = rezult;
+	printf("%u",rezult);
 }
 
 void rotate_right(unsigned int *number, int bits)
 {
 	/* TODO */
-	(void) number;
-	(void) bits;
+	unsigned int inumber = *number;
+	unsigned int rezult = (inumber >> bits) | (inumber << (sizeof(int)-bits));
+	*number = rezult;
+	printf("%u",rezult);
 }
 
 int main(void)
 {
 	/* TODO: Test functions */
+	unsigned int number, numberOfBits;
+	printf("Numar si nr de biti:\t");
+	scanf("%u%u",&number,&numberOfBits);
+	
+	rotate_left(&number,numberOfBits);
+	rotate_right(&number,numberOfBits);
+	//printf("Shiftare la stanga:\t%u\n",number);
 
+	//rotate_right(&number,numberOfBits);
+	//printf("Shiftare la dreapta:\t%u\n",number);
 	return 0;
 }
 
