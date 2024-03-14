c929651615a03bfd4fb6dfc60ffa275157725296
root <root@DESKTOP-2QHQ3TI>
2024-03-14 13:16:39 +0200

    Lab2 ex 4
    Signed-off-by: root <root@DESKTOP-2QHQ3TI>

--- laborator/content/operatii-memorie-gdb/4-find-max/find-max.c
+++ laborator/content/operatii-memorie-gdb/4-find-max/find-max.c
@@ -18,15 +18,19 @@
  */
 
 void *find_max(void *arr, int n, int element_size,
-				int (*compare)(const void *, const void *))
-{
-	void *max_elem = arr;
-
+               int (*compare)(const void *, const void *)) {
+    void *max_elem = arr;
 	(void) n;
 	(void) element_size;
 	(void) compare;
+    for (int i = 1; i < n; i++) {
+        void *cur_element = (char *)arr + i * element_size;
 
-	return max_elem;
+        if (compare(cur_element, max_elem))
+            max_elem = cur_element;
+    }
+
+    return max_elem;
 }
 
 /*
@@ -37,12 +41,19 @@ void *find_max(void *arr, int n, int element_size,
  * mare decat cea de la adresa lui b, in caz contrar returneaza 0.
  */
 
-int compare(const void *a, const void *b);
-
+int compare(const void *a, const void *b){
 /*
  * Se citeste de la tastatura un vector si se cere sa se afle
  * elementul maxim folosind functia find_max.
  */
+    const int *int_a = (const int *)a;
+    const int *int_b = (const int *)b;
+
+    if (*int_a > *int_b)
+        return 1;
+    else
+        return 0;
+}
 int main(void)
 {
 	int n;
@@ -53,7 +64,10 @@ int main(void)
 
 	for (int i = 0 ; i < n; ++i)
 		scanf("%d", &arr[i]);
+	int *max_elem = find_max(arr, n, sizeof(int), compare);
 
+    // Afisăm elementul maxim
+    printf("Elementul maxim din vector este: %d\n", *max_elem);
 	free(arr);
 	return 0;
 }
