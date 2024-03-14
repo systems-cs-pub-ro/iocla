013cfdd56c2edf518eaaf439ddfa6cab0621873c
alexserios <alexandru.iova@eestec.ro>
2024-03-14 08:57:25 +0200

    lab02: ex1; Add solution
    Signed-off-by: alexserios <alexandru.iova@eestec.ro>

--- laborator/content/operatii-memorie-gdb/1-iterate/iterate.c
+++ laborator/content/operatii-memorie-gdb/1-iterate/iterate.c
@@ -9,11 +9,25 @@
  * din 2 in 2 octeti si apoi din 4 in 4.
  */
 
-int main(void)
-{
-	int v[] = {0xCAFEBABE, 0xDEADBEEF, 0x0B00B135, 0xBAADF00D, 0xDEADC0DE};
+int main(void) {
+  int v[] = {0xCAFEBABE, 0xDEADBEEF, 0x0B00B135, 0xBAADF00D, 0xDEADC0DE};
 
-	(void) v;
+  (void)v;
+  int i;
+  int *pointerInt = (int *)(v);
+  short *pointerShort = (short *)(v);
+  char *pointerChar = (char *)(v);
+  for (i = 0; i < 5; i++) {
+    printf("%p: %d\n", &pointerInt[i], pointerInt[i]);
+  }
+  printf("De aici incepe din 2 in 2\n");
+  for (i = 0; i < 10; i++) {
+    printf("%p\n", &pointerShort[i]);
+  }
+  printf("De aici incepe din octet in octet\n");
+  for (i = 0; i < 20; i++) {
+    printf("%p\n", &pointerChar[i]);
+  }
 
-	return 0;
+  return 0;
 }
