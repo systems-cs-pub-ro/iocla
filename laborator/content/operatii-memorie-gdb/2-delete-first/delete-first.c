47ea7951b3c4b595e9abc8491a89784a946ec2ae
A0Dev0Knight <al.dev.knight@gmail.com>
2024-03-14 09:48:32 +0200

    Am terminat si exercitiul 2

--- laborator/content/operatii-memorie-gdb/2-delete-first/delete-first.c
+++ laborator/content/operatii-memorie-gdb/2-delete-first/delete-first.c
@@ -6,16 +6,15 @@
 
 char *delete_first(char *s, char *pattern){
 	
+	char *copy = strdup(s);
 	// la aceasta adresa incepe pattern in s prima data
-	char *s1 = strstr(s,pattern);
-	
+	char *s1 = strstr(copy,pattern);
 	// coopiez restul de sir s fara patttern
 	char *s2;
-	s2 = strdup(s1 + strlen(pattern));
-
+	s2 = s1 + strlen(pattern);
 	// copiez 
 	strcpy(s1,s2);
-	return s;
+	return copy;
 }
 
 int main(void)
@@ -26,16 +25,16 @@ int main(void)
 	 */
 
 	/*
-	 * RASPUNS:
+	 * RASPUNS: Nu se poate modifica direct stirngul s din main deoarece este constant
+	 * pentru a-l modifica este nevoie sa creez o copie.
 	*/
 	char *s = "Ana are mere";
 	char *pattern = "re";
 
 	(void) s;
 	(void) pattern;
-
 	// Decomentați linia după ce ați implementat funcția delete_first.
-	//printf("%s\n", delete_first(s, pattern));
+	printf("%s\n", delete_first(s, pattern));
 
 	return 0;
 }
