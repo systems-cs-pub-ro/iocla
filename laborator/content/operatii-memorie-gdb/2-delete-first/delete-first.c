14cd5823b6e545dd91270348b36b74ab1eafef09
root <root@DESKTOP-2QHQ3TI>
2024-03-14 13:15:52 +0200

    Lab2 ex2
    Signed-off-by: root <root@DESKTOP-2QHQ3TI>

--- laborator/content/operatii-memorie-gdb/2-delete-first/delete-first.c
+++ laborator/content/operatii-memorie-gdb/2-delete-first/delete-first.c
@@ -4,7 +4,21 @@
 #include <string.h>
 #include <stdlib.h>
 
-char *delete_first(char *s, char *pattern);
+char *delete_first(char *s, char *pattern){
+	int j = 0;
+	char *res = malloc(strlen(s)+1);
+	strcpy(res,s);
+	for (int i = 0; i <= strlen(s) - 2; i++){
+		if (*(s+i) == *(pattern) && *(s+i+1) == *(pattern+1) ){
+			for (j = i; j < strlen(s)-2; j++) *(res+j) = *(res+j+2);
+		
+	
+			res[j]='\0';
+			break;
+		}
+	}
+	return res;
+}
 
 int main(void)
 {
@@ -14,12 +28,13 @@ int main(void)
 	 */
 	char *s = "Ana are mere";
 	char *pattern = "re";
+	//printf("%d\n",strlen(pattern));
 
 	(void) s;
 	(void) pattern;
 
 	// Decomentați linia după ce ați implementat funcția delete_first.
-	// printf("%s\n", delete_first(s, pattern));
+	printf("%s\n", delete_first(s, pattern));
 
 	return 0;
 }
