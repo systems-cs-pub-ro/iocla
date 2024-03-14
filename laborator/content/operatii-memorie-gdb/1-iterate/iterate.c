a937438a52305cf78647f368db70ec387e024792
A0Dev0Knight <al.dev.knight@gmail.com>
2024-03-14 09:38:24 +0200

    Am facut ex 1 cum trebuie acum

--- laborator/content/operatii-memorie-gdb/1-iterate/iterate.c
+++ laborator/content/operatii-memorie-gdb/1-iterate/iterate.c
@@ -13,10 +13,21 @@ int main(void)
 {
 	int v[] = {0xCAFEBABE, 0xDEADBEEF, 0x0B00B135, 0xBAADF00D, 0xDEADC0DE};
 
+	short *s;
+	s=(short*)v;
+
 	int i;
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 10; i++)
 	{
-		printf("Adresa: %p\nValoare: %d\n\n",v+i,*(v+i));
+		printf("Adresa: %p\nValoare: %hx\n\n",s+i,*(s+i));
+	}
+	
+	char *c;
+	c=(char*)v;
+
+	for (i = 0; i < 20; i++)
+	{
+		printf("Adresa: %p\nValoare: %hhx\n\n",c+i,*(c+i));
 	}
 	
 	(void) v;
--- laborator/content/operatii-memorie-gdb/2-delete-first/delete-first.c
+++ laborator/content/operatii-memorie-gdb/2-delete-first/delete-first.c
@@ -4,7 +4,19 @@
 #include <string.h>
 #include <stdlib.h>
 
-char *delete_first(char *s, char *pattern);
+char *delete_first(char *s, char *pattern){
+	
+	// la aceasta adresa incepe pattern in s prima data
+	char *s1 = strstr(s,pattern);
+	
+	// coopiez restul de sir s fara patttern
+	char *s2;
+	s2 = strdup(s1 + strlen(pattern));
+
+	// copiez 
+	strcpy(s1,s2);
+	return s;
+}
 
 int main(void)
 {
@@ -12,6 +24,10 @@ int main(void)
 	 * TODO: Este corectă declarația variabilei s în contextul în care o să apelăm
 	 * funcția delete_first asupra sa? De ce? Modificați dacă este cazul.
 	 */
+
+	/*
+	 * RASPUNS:
+	*/
 	char *s = "Ana are mere";
 	char *pattern = "re";
 
@@ -19,7 +35,7 @@ int main(void)
 	(void) pattern;
 
 	// Decomentați linia după ce ați implementat funcția delete_first.
-	// printf("%s\n", delete_first(s, pattern));
+	//printf("%s\n", delete_first(s, pattern));
 
 	return 0;
 }
