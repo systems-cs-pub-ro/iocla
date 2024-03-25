a3004052fb3921be108520da0d7bef79642f82a0
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-03-25 18:42:56 +0200

    Lab04: Fix new line in printf
    Signed-off-by: Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>

--- laborator/content/toolchain-decompilare/2-warm-up-gotos/b-ignore_the_comments/ignore_the_comments.c
+++ laborator/content/toolchain-decompilare/2-warm-up-gotos/b-ignore_the_comments/ignore_the_comments.c
@@ -39,8 +39,7 @@ int main(void)
 
 	/* drunk, fix later */
 	while (ago < 0x2a) {
-		printf("http://stackoverflow.com/questions/184618/what-is-the-best
-			   -comment-in-source-code-you-have-ever-encountered\n");
+		printf("http://stackoverflow.com/questions/184618/what-is-the-best-comment-in-source-code-you-have-ever-encountered\n");
 		ago++;
 		continue; /* TODO: use goto for Pete's sake! */
 		printf("Fast inverse square root: %f\n", Q_rsqrt(x)); /* i'm sorry */
