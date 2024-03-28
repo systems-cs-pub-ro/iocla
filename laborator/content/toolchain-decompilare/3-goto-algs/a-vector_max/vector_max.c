795ac42d8c75f3d34429bf1faab714485ec313a9
alexiova264@gmail.com <alexiova264@gmail.com>
2024-03-28 08:48:12 +0200

    Added: ex3-a

--- laborator/content/toolchain-decompilare/3-goto-algs/a-vector_max/vector_max.c
+++ laborator/content/toolchain-decompilare/3-goto-algs/a-vector_max/vector_max.c
@@ -2,16 +2,21 @@
 
 #include <stdio.h>
 
-int main(void)
-{
-	int v[] = {4, 1, 2, -17, 15, 22, 6, 2};
-	int max;
-	int i;
+int main(void) {
+  int v[] = {4, 1, 2, 24, 15, 22, 6, 2};
+  int max;
+  int i;
 
-	/* TODO: Implement finding the maximum value in the vector */
-	max = v[0];
-	i = 1;
-
-	(void) i;
-	(void) max;
+  /* TODO: Implement finding the maximum value in the vector */
+  max = v[0];
+  i = 1;
+start:
+  if (i > 7) goto finish;
+  if (v[i] > max) max = v[i];
+  i++;
+  goto start;
+finish:
+  printf("%d\n", max);
+  (void)i;
+  (void)max;
 }
