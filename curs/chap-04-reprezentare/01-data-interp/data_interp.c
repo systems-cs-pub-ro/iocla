b7d7983f428a29cbeeb7b309421d873428cbe244
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2024-03-30 16:45:09 +0200

    task: Add descriptive variable names
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- curs/chap-04-reprezentare/01-data-interp/data_interp.c
+++ curs/chap-04-reprezentare/01-data-interp/data_interp.c
@@ -2,32 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-unsigned char c[8] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88};
-unsigned short s[4];
-unsigned int i[2];
-unsigned long long ll[1];
+unsigned char char_array[8] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88};
+unsigned short short_array[4];
+unsigned int int_array[2];
+unsigned long long ll_array[1];
 
 int main(void)
 {
 	size_t j;
 
-	memcpy(s, c, sizeof(c));
-	memcpy(i, c, sizeof(c));
-	memcpy(ll, c, sizeof(c));
+	memcpy(short_array, char_array, sizeof(char_array));
+	memcpy(int_array, char_array, sizeof(char_array));
+	memcpy(ll_array, char_array, sizeof(char_array));
 
 	printf("s:");
-	for (j = 0; j < sizeof(s) / sizeof(s[0]); j++)
-		printf(" 0x%04x", s[j]);
+	for (j = 0; j < sizeof(short_array) / sizeof(short_array[0]); j++)
+		printf(" 0x%04x", short_array[j]);
 	printf("\n");
 
 	printf("i:");
-	for (j = 0; j < sizeof(i) / sizeof(i[0]); j++)
-		printf(" 0x%08x", i[j]);
+	for (j = 0; j < sizeof(int_array) / sizeof(int_array[0]); j++)
+		printf(" 0x%08x", int_array[j]);
 	printf("\n");
 
 	printf("ll:");
-	for (j = 0; j < sizeof(ll) / sizeof(ll[0]); j++)
-		printf(" 0x%016llx", ll[j]);
+	for (j = 0; j < sizeof(ll_array) / sizeof(ll_array[0]); j++)
+		printf(" 0x%016llx", ll_array[j]);
 	printf("\n");
 
 	return 0;
