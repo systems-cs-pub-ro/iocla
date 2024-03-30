009383be6844f1d91c40c78f43d59553ad6fe15a
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2024-03-30 16:58:02 +0200

    task: Rename variables
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- curs/chap-04-reprezentare/03-signed-unsigned/signed_unsigned.c
+++ curs/chap-04-reprezentare/03-signed-unsigned/signed_unsigned.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-unsigned char uc = 0xaa;
-signed char sc = 0xaa;
+unsigned char unsigned_char = 0xaa;
+signed char signed_char = 0xaa;
 
-short int si = -1;
-unsigned int ui;
+short int short_int = -1;
+unsigned int unsigned_int;
 
 int main(void)
 {
-	printf("%hhu, %hhd\n", uc, sc);
+	printf("%hhu, %hhd\n", unsigned_char, signed_char);
 
-	ui = si;
-	printf("si (%p): 0x%08x, si: %u, si: %d\n", &si, si, si, si);
-	printf("ui (%p): 0x%08x, ui: %u, ui: %d\n", &ui, ui, ui, ui);
+	unsigned_int = short_int;
+	printf("short_int (%p): 0x%08x, short_int: %u, short_int: %d\n", &short_int, short_int, short_int, short_int);
+	printf("unsigned_int (%p): 0x%08x, unsigned_int: %u, unsigned_int: %d\n", &unsigned_int, unsigned_int, unsigned_int, unsigned_int);
 
 	return 0;
 }
