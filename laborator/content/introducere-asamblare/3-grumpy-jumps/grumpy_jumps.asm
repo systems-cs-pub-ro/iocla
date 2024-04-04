8286de47b0d8c035a480a5d486fa31a03a1768f7
Bobei Bogdan <bobei.bogdan.off@gmail.com>
2024-04-04 09:57:29 +0300

    Lb05 	-ex 4,5
    Signed-off-by: Bobei Bogdan <bobei.bogdan.off@gmail.com>

--- laborator/content/introducere-asamblare/3-grumpy-jumps/grumpy_jumps.asm
+++ laborator/content/introducere-asamblare/3-grumpy-jumps/grumpy_jumps.asm
@@ -9,8 +9,8 @@ section .text
     extern printf
 
 main:
-    mov eax, 0xdeadc0de         ; TODO3.1: modify eax register
-    mov ebx, 0x1337ca5e         ; TODO3.1: modify ebx register
+    mov eax, 0x2         ; TODO3.1: modify eax register
+    mov ebx, 0x3        ; TODO3.1: modify ebx register
     mov ecx, 0x5                ; hardcoded; DO NOT change
     cmp eax, ebx
     jns bad
--- laborator/content/introducere-asamblare/5-min/min.asm
+++ laborator/content/introducere-asamblare/5-min/min.asm
@@ -8,7 +8,11 @@ main:
     ;cele doua numere se gasesc in eax si ebx
     mov eax, 4
     mov ebx, 1
-    ; TODO: aflati minimul
+
+    cmp eax, ebx
+    jl print
+    xchg ebx, eax
+print:
     PRINTF32 `%d\n\x0`, eax ; afiseaza minimul
 
-    ret
+    rett
