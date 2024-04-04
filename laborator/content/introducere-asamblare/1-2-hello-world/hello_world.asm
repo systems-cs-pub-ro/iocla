c89a9befd81c55262a457a3aab5e465ccdb59549
Bobei Bogdan <bobei.bogdan.off@gmail.com>
2024-04-04 09:28:32 +0300

    Lab05 	-ex1-2 done
    Signed-off-by: Bobei Bogdan <bobei.bogdan.off@gmail.com>

--- laborator/content/introducere-asamblare/1-2-hello-world/hello_world.asm
+++ laborator/content/introducere-asamblare/1-2-hello-world/hello_world.asm
@@ -2,22 +2,31 @@
 
 section .data
     myString: db "Hello, World!", 0
+    ourString: db "Goodbye, World!", 0
 
 section .text
     global main
     extern printf
 
 main:
-    mov ecx, 6                      ; N = valoarea registrului ecx
-    mov eax, 1
+    mov ecx, 10                      ; N = valoarea registrului ecx
+    mov eax, 2
     mov ebx, 1
     cmp eax, ebx
-    je print                        ; TODO1: eax > ebx?
+    ja print                        ; TODO1: eax > ebx?
     ret
 
 print:
     PRINTF32 `%s\n\x0`, myString
-                                    ; TODO2.2: afisati "Hello, World!" de N ori
-                                    ; TODO2.1: afisati "Goodbye, World!"
+    PRINTF32 `%s\n\x0`, ourString
 
+    cmp ecx,ebx
+    je end
+    add ebx, 1
+    jmp print
+                                    ; TODO2.2: afisati "Hello, World!" de N ori
+                                    ; TODO2.1: afisati "Goodbye, World!
+    ret
+
+end:
     ret
