5a7696ac723028349239cd06eaaacf8ee18656d6
Alexandra Prigoreanu <aprigoreanu@stud.acs.upb.ro>
2024-04-04 09:04:34 +0300

    Lab 5: Solution for ex 1 & ex 2
    Signed-off-by: Alexandra Prigoreanu <aprigoreanu@stud.acs.upb.ro>

--- laborator/content/introducere-asamblare/1-2-hello-world/hello_world.asm
+++ laborator/content/introducere-asamblare/1-2-hello-world/hello_world.asm
@@ -2,22 +2,33 @@
 
 section .data
     myString: db "Hello, World!", 0
+    goodbye_string: db "Goodbye, world!", 0
 
 section .text
     global main
     extern printf
 
 main:
-    mov ecx, 6                      ; N = valoarea registrului ecx
-    mov eax, 1
+    mov ecx, 6
+    mov eax, 2
     mov ebx, 1
     cmp eax, ebx
-    je print                        ; TODO1: eax > ebx?
+    jg print
+    jmp print_goodbye
     ret
 
 print:
     PRINTF32 `%s\n\x0`, myString
-                                    ; TODO2.2: afisati "Hello, World!" de N ori
-                                    ; TODO2.1: afisati "Goodbye, World!"
+
+for:
+    cmp ecx, 1
+    je endfor
+    dec ecx
+    PRINTF32 `%s\n\x0`, myString
+    jmp for
+endfor:
+
+print_goodbye:
+    PRINTF32 `%s\n\x0`, goodbye_string
 
     ret
