88cf30f3c9ae69fc88b2846da7c62f2dec49edf2
Alexandra Prigoreanu <aprigoreanu@stud.acs.upb.ro>
2024-04-07 21:19:59 +0300

    Lab 5: Solution for ex 6
    Signed-off-by: Alexandra Prigoreanu <aprigoreanu@stud.acs.upb.ro>

--- laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
+++ laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
@@ -6,7 +6,18 @@ section .text
 
 main:
     mov eax, 7       ; vrem sa aflam al N-lea numar; N = 7
-
+    mov ecx, 2
+    mov ebx, 0 ; ebx retine valoarea lui f(i-2)
+    mov edx, 1 ; edx retine valoarea lui f(i-1)
     ; TODO: calculati al N-lea numar fibonacci (f(0) = 0, f(1) = 1)
 
-    ret
+begin:
+   cmp eax, 1
+   je print
+   add ebx, edx
+   xchg ebx,edx
+   dec eax
+   jmp begin
+print:
+   PRINTF32 `%u\n\x0`, edx
+ret
