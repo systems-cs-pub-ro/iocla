5996fad6dd148c82b1cc76fff3175daebf73c18b
Alexandra Prigoreanu <aprigoreanu@stud.acs.upb.ro>
2024-04-21 11:53:26 +0300

    Lab 7: Solution for ex 5 - segmentation fault after first occurence
    Signed-off-by: Alexandra Prigoreanu <aprigoreanu@stud.acs.upb.ro>

--- laborator/content/structuri-vectori-siruri/5-find-substring/find-substring.asm
+++ laborator/content/structuri-vectori-siruri/5-find-substring/find-substring.asm
@@ -14,6 +14,34 @@ main:
     mov ebp, esp
 
     ; TODO: Print the start indices for all occurrences of the substring in source_text
+    xor ecx, ecx
+    mov eax, source_text
+loop:
+    mov ebx, substring
 
+strstr_loop:
+    cmp byte [ebx], 0
+    je found_str
+    mov dl, [eax]
+    cmp byte [ebx], dl
+    jne exit_loop
+    inc eax
+    inc ebx
+    inc ecx
+    jmp strstr_loop
+
+found_str:
+    push ecx
+    push print_format
+    call printf
+    add esp, 8
+
+exit_loop:
+    inc eax
+    cmp byte [eax], 0
+    je end_label
+    jmp loop
+
+end_label:
     leave
     ret
