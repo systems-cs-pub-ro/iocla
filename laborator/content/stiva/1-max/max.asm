b3d6fe3ca9755f80e72c718a041194516d46a842
MadlenaB <madlenablajin@Madlenas-MacBook-Pro.local>
2024-04-24 20:01:54 +0300

    This is the solution for ex 1. Comparing two registers using stack functions
    Signed-off-by: MadlenaB <madlenablajin@Madlenas-MacBook-Pro.local>

--- laborator/content/stiva/1-max/max.asm
+++ laborator/content/stiva/1-max/max.asm
@@ -9,8 +9,15 @@ main:
     mov eax, 1
     mov ebx, 4 
 
+    cmp eax, ebx
+    ja print_maxim 
+    push eax
+    push ebx
+    pop eax
+    pop ebx
+
     ; TODO: get maximum value. You are only allowed to use one conditional jump and push/pop instructions.
-
+print_max:
     PRINTF32 `Max value is: %d\n\x0`, eax ; print maximum value
-
+    
     ret
