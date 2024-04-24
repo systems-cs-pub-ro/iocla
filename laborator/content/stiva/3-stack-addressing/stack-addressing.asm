32912a575a27b389b61f330dfe54f9690e2e7eb0
MadlenaB <madlenablajin@Madlenas-MacBook-Pro.local>
2024-04-24 20:05:01 +0300

    This is the solution for ex3. We do not use push, but mov
    Signed-off-by: MadlenaB <madlenablajin@Madlenas-MacBook-Pro.local>

--- laborator/content/stiva/3-stack-addressing/stack-addressing.asm
+++ laborator/content/stiva/3-stack-addressing/stack-addressing.asm
@@ -12,13 +12,21 @@ main:
     ; TODO 1: replace every push by an equivalent sequence of commands (use direct addressing of memory. Hint: esp)
     mov ecx, NUM
 push_nums:
-    push ecx
+    sub esp, 4
+    mov dword[esp], ecx
     loop push_nums
 
-    push 0
-    push "mere"
-    push "are "
-    push "Ana "
+    sub esp, 4
+    mov dword[esp], 0
+
+    sub esp, 4
+    mov dword[esp], "mere"
+
+    sub esp, 4
+    mov dword[esp], "are"
+
+    sub esp, 4
+    mov dword[esp], "Ana"
 
     lea esi, [esp]
     PRINTF32 `%s\n\x0`, esi
@@ -26,13 +34,22 @@ push_nums:
     ; TODO 2: print the stack in "address: value" format in the range of [ESP:EBP]
     ; use PRINTF32 macro - see format above
 
-    ; TODO 3: print the string
-
     ; TODO 4: print the array on the stack, element by element.
+    mov eax, ebp
+print_value:
+    PRINTF32 `0x%x: 0x%x\n\x0`, eax, [eax]
+
+    sub eax, 4
+    cmp eax, esp
+    jge print_value
+
+
+    ; TODO 3: print the string
+    lea esi, [esp]
+    PRINTF32 `%s\n\x0`, esi
 
     ; restore the previous value of the EBP (Base Pointer)
-    mov esp, ebp
-
+     mov esp, ebp
     ; exit without errors
     xor eax, eax
     ret
