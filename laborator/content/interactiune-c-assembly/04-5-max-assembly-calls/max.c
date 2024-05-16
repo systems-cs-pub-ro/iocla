c3a2a2f7849aea36a494860b9d2faa95850e370e
Carpinisan George <gcarpinisan.04@gmail.com>
2024-05-16 12:15:38 +0300

    Solved ex5
    Pushed the remaining params and updated esp after each function call
    
    Signed-off-by: Carpinisan George <gcarpinisan.04@gmail.com>

--- laborator/content/interactiune-c-assembly/04-5-max-assembly-calls/max.c
+++ laborator/content/interactiune-c-assembly/04-5-max-assembly-calls/max.c
@@ -2,14 +2,20 @@
 
 #include <stdio.h>
 
-unsigned int get_max(unsigned int *arr, unsigned int len)
+unsigned int get_max(unsigned int *arr, unsigned int len, unsigned int *pos)
 {
 	unsigned int max = 0;
 	size_t i;
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len; i++) {
 		if (max < arr[i])
 			max = arr[i];
+    }
+
+    for (i = 0; i < len; i++) {
+        if (arr[i] == max)
+            *pos = i;
+    }
 
 	return max;
 }
