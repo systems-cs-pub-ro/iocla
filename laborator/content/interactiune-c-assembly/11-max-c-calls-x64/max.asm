03459a398e1ee94a3a0745eeacb3acac61bca81f
Carpinisan George <gcarpinisan.04@gmail.com>
2024-05-16 12:24:31 +0300

    Solved ex11
    Changed each 32bit register to the 64bit equivalent
    
    Signed-off-by: Carpinisan George <gcarpinisan.04@gmail.com>

--- laborator/content/interactiune-c-assembly/11-max-c-calls-x64/max.asm
+++ laborator/content/interactiune-c-assembly/11-max-c-calls-x64/max.asm
@@ -5,20 +5,20 @@ section .text
 global get_max
 
 get_max:
-	push ebp
-	mov ebp, esp
+	push rbp
+	mov rbp, rsp
 
 	; [ebp+8] is array pointer
 	; [ebp+12] is array length
 
-	mov ebx, [ebp+8]
-	mov ecx, [ebp+12]
-	xor eax, eax
+	mov rbx, rdi
+	mov rcx, rsi
+	xor rax, rax
 
 compare:
-	cmp eax, [ebx+ecx*4-4]
+	cmp eax, [rbx+rcx*4-4]
 	jge check_end
-	mov eax, [ebx+ecx*4-4]
+	mov eax, [rbx+rcx*4-4]
 check_end:
 	loopnz compare
 
