1b63547afb0094840bf5a68d7de55ed7a62d0fb8
Carpinisan George <gcarpinisan.04@gmail.com>
2024-05-16 12:26:11 +0300

    Solved ex12
    Put the arguments in rsi and rdi before the function call
    
    Signed-off-by: Carpinisan George <gcarpinisan.04@gmail.com>

--- laborator/content/interactiune-c-assembly/12-max-assembly-calls/main.asm
+++ laborator/content/interactiune-c-assembly/12-max-assembly-calls/main.asm
@@ -21,19 +21,19 @@ main:
     ; Compute length in eax.
     ; Divide by 4 (we are using integer data type of 4 bytes) by
     ; using shr 2 (shift right with 2 bits).
+    xor rax, rax
     mov eax, len
     shr eax, 2
 
-    push rax
-    push arr
+    mov rsi, rax
+    mov rdi, arr
     call get_max
-    add esp, 8
 
     ; Print max.
-    push rax
-    push print_format
+    mov rsi, rax
+    xor rdi, rdi
+    mov rdi, print_format
     call printf
-    add esp, 8
 
     leave
     ret
